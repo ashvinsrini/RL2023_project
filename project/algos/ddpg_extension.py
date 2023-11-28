b98bb852fd3ddc4f1e1355d3c072281e62dbdef3
Srinivasan Ashvin <sriniva3@login3.triton.aalto.fi>
2023-11-28 15:52:00 +0200

    Adding TD3 implementation for task 2. For the middle environment the mean TD3 value is 0.833 improvment over using DDPG which yielded a mean of 0.73. Check project notebook for results.

--- project/algos/ddpg_extension.py
+++ project/algos/ddpg_extension.py
@@ -12,5 +12,78 @@ from pathlib import Path
 def to_numpy(tensor):
     return tensor.cpu().numpy().flatten()
 
+#class DDPGExtension(DDPGAgent):
+    #pass
+    
 class DDPGExtension(DDPGAgent):
-    pass
+    def __init__(self, config=None):
+        super(DDPGExtension, self).__init__(config)
+        self.update_counter = 0
+        # Additional initialization or modifications can be done here if needed.
+        state_dim = self.observation_space_dim
+        self.action_dim = self.action_space_dim
+        self.q2 = Critic(state_dim, self.action_dim).to(self.device)
+        self.q2_target = copy.deepcopy(self.q2)
+        self.q2_optim = torch.optim.Adam(self.q2.parameters(), lr=float(self.lr))
+        
+        # Other TD3-specific attributes, if any
+        self.policy_delay = 8
+        self.policy_noise = 0.3
+        #self.noise_clip = self.cfg.noise_clip        
+        
+
+    def _update(self):
+        self.update_counter += 1  # Increment the update counter
+        #print('update_counter', self.update_counter)
+        # get batch data
+        batch = self.buffer.sample(self.batch_size, device=self.device)
+        state, action, next_state, reward, not_done = batch.state, batch.action, batch.next_state, batch.reward, batch.not_done
+
+        # compute current q values for both critics
+        current_q1 = self.q(state, action)
+        current_q2 = self.q2(state, action)
+
+        # compute target q values using the target actor and both target critics
+        with torch.no_grad():
+            next_action = self.pi_target(next_state)
+            #noise = torch.clamp(torch.normal(0, self.policy_noise, size=next_action.shape).to(self.device), -self.noise_clip, self.noise_clip)
+            #next_action = torch.clamp(next_action + noise, -self.max_action, self.max_action)
+            
+            next_action = next_action + torch.normal(0, self.policy_noise, size=next_action.shape).to(self.device)
+            next_action = torch.clamp(next_action, -self.max_action, self.max_action)
+            
+            target_q1 = self.q_target(next_state, next_action) * not_done
+            target_q2 = self.q2_target(next_state, next_action) * not_done
+            target_q = torch.min(target_q1, target_q2)
+            target_q = reward + self.gamma * target_q
+
+        # compute critic losses for both critics
+        critic_loss1 = F.mse_loss(current_q1, target_q)
+        critic_loss2 = F.mse_loss(current_q2, target_q)
+
+        # optimize both critics
+        self.q_optim.zero_grad()
+        critic_loss1.backward()
+        self.q_optim.step()
+
+        self.q2_optim.zero_grad()
+        critic_loss2.backward()
+        self.q2_optim.step()
+
+        # delay policy update
+        if self.update_counter % self.policy_delay == 0:
+            # compute actor loss
+            #print('updating policy, update_counter:', self.update_counter)
+            actor_loss = -self.q(state, self.pi(state)).mean()
+
+            # optimize actor
+            self.pi_optim.zero_grad()
+            actor_loss.backward()
+            self.pi_optim.step()
+
+            # update target networks
+            cu.soft_update_params(self.q, self.q_target, self.tau)
+            cu.soft_update_params(self.q2, self.q2_target, self.tau)
+            cu.soft_update_params(self.pi, self.pi_target, self.tau)
+
+        return {}
